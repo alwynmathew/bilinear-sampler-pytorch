264191e08de054aed210328bd22a38d69c361b3a
Alwyn Mathew <alwynmathew.90@gmail.com>
2018-06-16 16:17:18 +0530

    Update bilinear.py

--- bilinear.py
+++ bilinear.py
@@ -11,8 +11,8 @@ def image_warp(img, depth, padding_mode='zeros'):
     pixel_coords = torch.stack((j_range, i_range), dim=1).float().cuda()  # [1, 2, H, W]
     batch_pixel_coords = pixel_coords[:,:,:,:].expand(b,2,h,w).contiguous().view(b, 2, -1)  # [B, 2, H*W]
 
-    X = batch_pixel_coords[:, 0]  + depth.contiguous().view(b,-1) # [B, H*W]
-    Y = batch_pixel_coords[:, 1]
+    X = batch_pixel_coords[:, 0, :]  + depth.contiguous().view(b,-1) # [B, H*W]
+    Y = batch_pixel_coords[:, 1, :]
 
     X_norm = X 
     Y_norm = Y  
