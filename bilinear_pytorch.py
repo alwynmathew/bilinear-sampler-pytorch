8ae0dad35a40f7ee8199f25409e5f57a48125af6
Alwyn Mathew <alwynmathew.90@gmail.com>
2018-08-13 20:25:59 +0530

    Update bilinear_pytorch.py
    fixed issues, all wrap mode works now

--- bilinear_pytorch.py
+++ bilinear_pytorch.py
@@ -1,92 +1,72 @@
 from __future__ import absolute_import, division, print_function
 import torch
-import torch.nn.functional as F
-from torch.autograd import Variable
+from torch.nn.functional import pad
 
-def bilinear_sampler_1d_h(input_images, x_offset, **kwargs):
+def apply_disparity(input_images, x_offset, wrap_mode='border', tensor_type = 'torch.cuda.FloatTensor'):
+    num_batch, num_channels, height, width = input_images.size()
 
-	def _repeat(x, n_repeats):
-		rep = x.unsqueeze(1).repeat(1, n_repeats)
-		return rep.view(-1)
+    # Handle both texture border types
+    edge_size = 0
+    if wrap_mode == 'border':
+        edge_size = 1
+        # Pad last and second-to-last dimensions by 1 from both sides
+        input_images = pad(input_images, (1, 1, 1, 1))
+    elif wrap_mode == 'edge':
+        edge_size = 0
+    else:
+        return None
 
-	def _interpolate(im, x, y):
-		
-		_edge_size = 0
-		if _wrap_mode == 'border':
-			_edge_size = 1
-			im = F.pad(im,(0,0,1,1,1,1,0,0), 'constant')
-			x = x + _edge_size
-			y = y + _edge_size
-		elseif _wrap_mode == 'edge':
-			_edge_size = 0
-		else:
-			return None
+    # Put channels to slowest dimension and flatten batch with respect to others
+    input_images = input_images.permute(1, 0, 2, 3).contiguous()
+    im_flat = input_images.view(num_channels, -1)
 
-		x = torch.clamp(x, 0.0,  _width_f - 1)
+    # Create meshgrid for pixel indicies (PyTorch doesn't have dedicated
+    # meshgrid function)
+    x = torch.linspace(0, width - 1, width).repeat(height, 1).type(tensor_type).to(opt.gpu_ids)
+    y = torch.linspace(0, height - 1, height).repeat(width, 1).transpose(0, 1).type(tensor_type).to(opt.gpu_ids)
+    # Take padding into account
+    x = x + edge_size
+    y = y + edge_size
+    # Flatten and repeat for each image in the batch
+    x = x.view(-1).repeat(1, num_batch)
+    y = y.view(-1).repeat(1, num_batch)
 
-		x0_f = torch.floor(x)
-		y0_f = torch.floor(y)
-		x1_f = x0_f + 1
+    # Now we want to sample pixels with indicies shifted by disparity in X direction
+    # For that we convert disparity from % to pixels and add to X indicies
+    x = x + x_offset.contiguous().view(-1) * width
+    # Make sure we don't go outside of image
+    x = torch.clamp(x, 0.0, width - 1 + 2 * edge_size)
+    # Round disparity to sample from integer-valued pixel grid
+    y0 = torch.floor(y)
+    # In X direction round both down and up to apply linear interpolation
+    # between them later
+    x0 = torch.floor(x)
+    x1 = x0 + 1
+    # After rounding up we might go outside the image boundaries again
+    x1 = x1.clamp(max=(width - 1 + 2 * edge_size))
 
-		x0 = x0_f.type(torch.FloatTensor)
-		y0 = y0_f.type(torch.FloatTensor)
+    # Calculate indices to draw from flattened version of image batch
+    dim2 = (width + 2 * edge_size)
+    dim1 = (width + 2 * edge_size) * (height + 2 * edge_size)
+    # Set offsets for each image in the batch
+    base = dim1 * torch.arange(num_batch).type(tensor_type).to(opt.gpu_ids)
+    base = base.view(-1, 1).repeat(1, height * width).view(-1)
+    # One pixel shift in Y  direction equals dim2 shift in flattened array
+    base_y0 = base + y0 * dim2
+    # Add two versions of shifts in X direction separately
+    idx_l = base_y0 + x0
+    idx_r = base_y0 + x1
 
-		min_val = _width_f - 1
-		scalar = torch.FloatTensor([min_val]).cuda()
+    # Sample pixels from images
+    pix_l = im_flat.gather(1, idx_l.repeat(num_channels, 1).long())
+    pix_r = im_flat.gather(1, idx_r.repeat(num_channels, 1).long())
 
-		x1 = torch.min(x1_f, scalar)
-		x1 = x1.type(torch.FloatTensor)
-		dim2 = _width
-		dim1 = _width * _height
-		base = _repeat(torch.arange(_num_batch) * dim1, _height * _width)
+    # Apply linear interpolation to account for fractional offsets
+    weight_l = x1 - x
+    weight_r = x - x0
+    output = weight_l * pix_l + weight_r * pix_r
 
-		base_y0 = base + y0 * dim2
-		idx_l = base_y0 + x0
-		idx_r = base_y0 + x1
-		idx_l = Variable(idx_l.type(torch.cuda.LongTensor))
-		idx_r = Variable(idx_r.type(torch.cuda.LongTensor))
+    # Reshape back into image batch and permute back to (N,C,H,W) shape
+    output = output.view(num_channels, num_batch, height, width).permute(1,0,2,3)
 
-		im_flat = im.contiguous().view(-1, _num_channels)
-		pix_l = torch.gather(im_flat, 0, idx_l.repeat(_num_channels).view(-1, _num_channels))
-		pix_r = torch.gather(im_flat, 0, idx_r.repeat(_num_channels).view(-1, _num_channels)) 
-
-		weight_l = Variable((x1_f - x).unsqueeze(1))
-		weight_r = Variable((x - x0_f).unsqueeze(1))
-
-		return weight_l * pix_l + weight_r * pix_r
-
-	def _transform(input_images, x_offset):
-
-		a = torch.linspace(0.0, _width_f -1.0, _width).cuda()
-		b = torch.linspace(0.0, _height_f -1.0, _height).cuda()
-
-		x_t = a.repeat(_height)
-		y_t = b.repeat(_width,1).t().contiguous().view(-1)
-
-		x_t_flat = x_t.repeat(_num_batch, 1)
-		y_t_flat = y_t.repeat(_num_batch, 1)
-
-		x_t_flat = x_t_flat.view(-1)
-		y_t_flat = y_t_flat.view(-1)
-
-		x_t_flat = x_t_flat + x_offset.data.contiguous().view(-1) * _width_f
-
-		input_transformed = _interpolate(input_images, x_t_flat, y_t_flat)
-
-		output = input_transformed.view(_num_batch, _num_channels, _height, _width)
-
-		return output
-
-	_num_batch    = input_images.size(0)
-	_num_channels = input_images.size(1)
-	_height       = input_images.size(2)
-	_width        = input_images.size(3)
-
-	_height_f = float(_height)
-	_width_f = float(_width)
-
-	_wrap_mode = wrap_mode
-
-	output = _transform(input_images, x_offset)
-
-	return output
+    return output
